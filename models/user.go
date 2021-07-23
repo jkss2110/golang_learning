5bead091e11cbfbbab214558d0915db02c6fd7ab
jayakrishnan sunil <jayakrishnan.sunil@sap.com>
2021-07-23 07:08:31 +0530

    changes

--- models/user.go
+++ models/user.go
@@ -49,12 +49,12 @@ func UpdateUser(u User) (User, error) {
 	return User{}, fmt.Errorf("User update for '%v' failed", u.ID)
 }
 
-func RemoveUserById(id int) (User, error) {
+func RemoveUserById(id int) error {
 	for i, u := range users {
 		if u.ID == id {
 			users = append(users[:i], users[i+1:]...)
 			return nil
 		}
 	}
-	return User{}, fmt.Errorf("User missing for '%v'", id)
+	return fmt.Errorf("User missing for '%v'", id)
 }
